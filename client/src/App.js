9ad8078e2942e0be6d3b4f296d888dbe2a26a662
Kit <djneel.nz@gmail.com>
2023-02-22 22:53:59 +0300

    added main components and pages link and routes to app

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Main from './pages/Main/Main';
+import Price from './components/Price/Price'
+import Team from './components/Team/Team'
+import About from './components/About/About'
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/price" element={<Price />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function NavBar() {
+  return (
+    <div>
+      <Link>Main</Link>
+      <Link>About</Link>
+      <Link>Price</Link>
+      <Link>Team</Link>
+      <Link>Contats</Link>
+    </div>
+  )
+}
