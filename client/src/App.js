436f35d274018bf3f35fe631d507b7b9fe4af584
Kit <djneel.nz@gmail.com>
2023-02-22 22:31:09 +0300

    init project

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Main from './pages/Main/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
