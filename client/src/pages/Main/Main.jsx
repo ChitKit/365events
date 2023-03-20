dea66dbf2ce96a2fe132f83477dcc05db37c322f
Kit <djneel.nz@gmail.com>
2023-03-20 21:35:21 +0300

    fixed main page style

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -5,9 +5,9 @@ export default function Main() {
   return (
    <div className="MainPage">
       <h1 className="MainPageH1">Главная страница агенства 365:events</h1>
-      <h3 className="MainPageH3">
+      <h2 className="MainPageH2">
         Доставляем с любовью самые тёплые и волшебные праздники на дом, в детский садик, школу или офис! 365 дней в году.
-      </h3>
+      </h2>
    </div>
 
   )
