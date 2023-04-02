e9ab24d9f24d4dac9cd12a7a6f706e8b0adcc06e
ChitKit <you@example.com>
2023-04-02 10:51:37 +0300

    in work process navigationBar on tree

--- client/src/Layout/MainLayout.jsx
+++ client/src/Layout/MainLayout.jsx
@@ -5,28 +5,58 @@ import { Link } from 'react-router-dom'
 import './MainLayout.css'
 
 export default function MainLayout() {
-    
   const [currentImg, setCurrentImg] = useState(undefined)
   const [currentColorForImg, setCurrentColorForImg] = useState(undefined)
+  const [loader, setLoader] = useState(false)
 
-
-  useEffect(() => {
-    setCurrentImg('img/Winter.jpeg')
-    setCurrentColorForImg('(197,222,229)')
-  }, [])
+  setInterval(() => {
+    const choicerTime = new Date().getSeconds()
+    setLoader(true)
+    if (choicerTime < 15) {
+      setCurrentImg('img/Winter.jpeg')
+      setCurrentColorForImg('(197,222,229)')
+      setLoader(false)
+    } else if (choicerTime > 15 && choicerTime < 30) {
+      setCurrentImg('img/Summer.jpeg')
+      setCurrentColorForImg('(243,219,121)')
+      setLoader(false)
+    } else if (choicerTime > 30 && choicerTime < 45) {
+      setCurrentImg('img/Spring.jpeg')
+      setCurrentColorForImg(`(194,230,122)`)
+      setLoader(false)
+    } 
+    else if (choicerTime > 45) {
+      setCurrentImg('img/Autumn.jpeg')
+      setCurrentColorForImg('(241,177,139)')
+      setLoader(false)
+    }
+  },1000)
+  
+  // useEffect(() => {
+  //   setCurrentImg('img/Winter.jpeg')
+  //   setCurrentColorForImg('(197,222,229)')
+  // }, [])
 
   return (
     <>
-        <div className="NavigationApp" style={{right:'15%', position:'fixed', zIndex:'2', top:'43%'}}>
-            <Link to={"/"}>Главная</Link>
-            <Link to={"/about"}>О нас</Link>
-            <Link to={"/price"}>Услуги</Link>
-            <Link to={"/team"}>Команда</Link>
-            <Link to={"/contacts"}>Контакты</Link>
+      {loader ? 
+        <div style={{width:'100%', height:'100%', display:'flex', justifyContent:'center', alignItems:'center'}}>
+          <img className="Loader" src="logo512.png" alt="logo" />
         </div>
-        <div style={{height: '100%', width:'100%', display:'flex', justifyContent:'flex-end',alignItems:'flex-end', position:'fixed', zIndex:'1'}} >
-            <img style={{width:'100%', height:'100%', zIndex:'0'}} src={`${currentImg}`} alt="main_pic" ></img>
+        :
+        <div style={{backgroundColor:`rgb${currentColorForImg}`, width:'100%', height: '100%'}}>
+            <div className="NavigationApp" style={{right:'15%', position:'fixed', zIndex:'2', top:'43%'}}>
+                <Link to={"/"}>Главная</Link>
+                <Link to={"/about"}>О нас</Link>
+                <Link to={"/price"}>Услуги</Link>
+                <Link to={"/team"}>Команда</Link>
+                <Link to={"/contacts"}>Контакты</Link>
+            </div>
+            <div style={{height: '100%', width:'100%', display:'flex', justifyContent:'flex-end',alignItems:'flex-end', position:'fixed', zIndex:'1'}} >
+                <img style={{zIndex:'0'}} src={`${currentImg}`} alt="main_pic" ></img>
+            </div>
         </div>
+      }
     </>
   )
 }
--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -3,32 +3,6 @@ import './Main.css'
 import MainLayout from '../../Layout/MainLayout'
 
 export default function Main() {
-  // const [currentImg, setCurrentImg] = useState(undefined)
-  // const [currentColorForImg, setCurrentColorForImg] = useState(undefined)
-
-
-  // useEffect(() => {
-  //   setCurrentImg('img/Winter.jpeg')
-  //   setCurrentColorForImg('(197,222,229)')
-  // }, [])
-  
-
-  // setInterval(() => {
-    // if (choicerTime < 15) {
-      // setCurrentImg('img/Winter.jpeg')
-      // setCurrentColorForImg('(197,222,229)')
-  //   } else if (choicerTime > 15 && choicerTime < 30) {
-  //     setCurrentImg('img/Summer.jpeg')
-  //     setCurrentColorForImg('(243,219,121)')
-  //   } else if (choicerTime > 30 && choicerTime < 45) {
-  //     setCurrentImg('img/Spring.jpeg')
-  //     setCurrentColorForImg(`(194,230,122)`)
-  //   } 
-  //   else if (choicerTime > 45) {
-  //     setCurrentImg(undefined)
-  //     setCurrentColorForImg('(243,219,121)')
-  //   }
-  // },1000)
 
   return (
     <MainLayout>
