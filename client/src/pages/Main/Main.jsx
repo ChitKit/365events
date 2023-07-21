8efc155ebf48908a16c4b0075cb0eabdf05db37e
ChitKit <you@example.com>
2023-07-21 23:42:44 +0300

    fix time animation

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -13,35 +13,34 @@ export default function Main({}) {
     return (
         <div className="Application-Content">
             <div id="main" className="MainPage">
+                <h1 style={{ position: "absolute", top:'5%', zIndex: "2" }}
+                    className="MainPageH1-Top">
+                    Агентство праздников
+                </h1>
                 <div className="MainPageH1Div">
                     <h1
                         style={{ position: "relative", zIndex: "2" }}
-                        className="MainPageH1"
+                        className="MainPageH1-Logo"
                     >
-                        Агентство праздников <br />
                         365:events
                     </h1>
                 </div>
-
                 <div className="MainPageH2Div">
                     <div className="MainPageH2">
-                        <br />
+                            <br />
                             <span>Доставляем </span>
                             <span> с </span>
                             <span> любовью </span>
-                            <br />
                             <span> самые </span>
                             <span> тёплые </span>
                             <span> и </span>
                             <span> волшебные </span>
                             <span> праздники </span>
-                            <br />
                             <span> 365 </span>
                             <span> дней </span>
                             <span> в </span>
                             <span> году </span>
                             <span>.</span>
-                            <br />
                     </div>
                 </div>
             </div>
